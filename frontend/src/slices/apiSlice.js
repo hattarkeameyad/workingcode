ccf178e6c908029f8beb1c7269a62d242864b316
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-02-05 10:06:43 +0530

    backend front end working code

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,11 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+// const baseQuery = fetchBaseQuery({ baseUrl: 'import.meta.env.VITE_API_URL' });
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_URL || 'http://18.234.218.167:5000',
+  credentials: 'include',
+});
 
 export const apiSlice = createApi({
   baseQuery,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://18.234.218.167:5000',
         changeOrigin: true,
       },
     },
